9c179d98cdd4a2fcb3a95a9ddd4d67e1c8b0e05f
Mike Dougherty <miked@threedev.com>
2020-06-29 06:34:02 -0700

    Update the shell script to run the Javascript

--- .github/actions/docker-env/entrypoint.sh
+++ .github/actions/docker-env/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh -l
 
-echo "Show contents of $(pwd):"
-ls -la
-echo "Finding all files..."
-find . -type f
+#echo "Show contents of $(pwd):"
+#ls -la
+#echo "Finding all files..."
+#find . -type f
+cd .github/actions/docker-env
+npm install
+node main.js
