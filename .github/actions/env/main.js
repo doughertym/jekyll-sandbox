dcf3dd497373626788dd85d1a67bf34429ec02f0
Mike Dougherty <miked@threedev.com>
2020-06-28 18:22:49 -0700

    Show environment

--- .github/actions/env/main.js
+++ .github/actions/env/main.js
@@ -2,6 +2,7 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
+    console.log(JSON.stringify(process.env, undefined, 2));
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
     console.log(`The event payload: ${payload}`);
